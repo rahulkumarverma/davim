6f47b4d17717edcefca0192037febd0a8ade934b
rahulkumarverma <shellcode14@gmail.com>
2014-09-18 16:46:39 +0530

    first commit

---
+++ 18thsept/binary.java
@@ -0,0 +1,12 @@
+class binary
+{
+public static void main(String args[])
+{
+//int n=Integer.parseInt(args[0]);
+//System.out.println(Byte.MIN_VALUE);
+//System.out.println(Byte.MAX_VALUE);
+
+byte b1=128;
+System.out.println(b1);
+}
+}
---
+++ 18thsept/one.java
@@ -0,0 +1,16 @@
+class one
+{
+public static void main(String args[])
+{
+int n=5;
+for(n=5;n>=1;n--)
+
+{
+
+for(int k=n;k>=1;k--)
+System.out.print("*");
+}
+System.out.println("");
+
+}
+}
